b119d06c8c71abc5c7722e8f86af81e192c84575
gmsea <gmatos@hotmail.com.br>
2024-11-04 17:48:56 -0300

    alterado template do AppComponent para incluir o LivroLista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component'; // ajuste o caminho conforme necessário
 import { ControleEditoraService } from './controle-editora.service';
 import { ControleLivrosService } from './controle-livros.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LivroListaComponent
   ],
   imports: [
     BrowserModule,
