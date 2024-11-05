6e905acf6bcea621cb5df29d0ee42c90a7c3aa05
gmsea <gmatos@hotmail.com.br>
2024-11-05 14:48:07 -0300

    Adicionado exemplos de livros reais

--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -5,22 +5,21 @@ import { Livro } from './livro';
   providedIn: 'root'
 })
 export class ControleLivrosService {
-   private livros: Array<Livro> = [
+  private livros: Array<Livro> = [
     { codigo: 1, codEditora: 1, titulo: 'Livro A', resumo: 'Resumo do Livro A', autores: ['Autor A1', 'Autor A2'] },
     { codigo: 2, codEditora: 2, titulo: 'Livro B', resumo: 'Resumo do Livro B', autores: ['Autor B1'] },
-    { codigo: 3, codEditora: 3, titulo: 'Livro C', resumo: 'Resumo do Livro C', autores: ['Autor C1', 'Autor C2'] }
-  ];
+    { codigo: 3, codEditora: 3, titulo: 'Livro C', resumo: 'Resumo do Livro C', autores: ['Autor C1', 'Autor C2'] },
 
-  constructor() {}
+  ];
 
   obterLivros(): Array<Livro> {
     return this.livros;
   }
 
-  incluir(novoLivro: Livro): void {
-    const novoCodigo = Math.max(...this.livros.map(livro => livro.codigo), 0) + 1;
-    novoLivro.codigo = novoCodigo;
-    this.livros.push(novoLivro);
+  incluir(livro: Livro): void {
+    const novoCodigo = Math.max(...this.livros.map(l => l.codigo)) + 1;
+    livro.codigo = novoCodigo;
+    this.livros.push(livro);
   }
 
   excluir(codigo: number): void {
